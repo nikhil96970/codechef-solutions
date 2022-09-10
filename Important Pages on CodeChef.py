22fe35a47fec27222546bcd9e7458dfeeba903ce
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-09-10 21:49:05 +0530

    Create Important Pages on CodeChef.py

---
+++ Important Pages on CodeChef.py
@@ -0,0 +1,8 @@
+# cook your dish here
+a,b=map(int,input().split(' '))
+if (a==0):
+    print("https://www.codechef.com/practice")
+elif(a==1 and b==0):
+    print("https://www.codechef.com/contests")
+else:
+    print("https://discuss.codechef.com")
