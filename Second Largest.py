d8c3c6a6a3c2c9fadd0d3d297b4334d18e43715e
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-09-10 23:24:46 +0530

    Create Second Largest.py

---
+++ Second Largest.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a,b,c=map(int,input().split())
+    if b<a<c or c<a<b:
+        print(a)
+    elif c<b<a or a<b<c:
+        print(b)
+    else:
+        print(c)
