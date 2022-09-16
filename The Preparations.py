9ee2a8cc857c0ef8a5be5b1f40097858299f000e
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-09-16 21:27:41 +0530

    Create The Preparations.py

---
+++ The Preparations.py
@@ -0,0 +1,14 @@
+# cook your dish here
+a=input()
+a=a.split()
+
+if(a[0]==a[1]):
+    print(a)
+if(a[0]==R or a[1]==R):
+    print("R")
+elif(a[0]==G and a[1]==G):
+    print("G")
+else:
+    print("B")
+   
+    
