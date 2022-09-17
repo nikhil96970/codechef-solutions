ce3a9131bfefe1a933ab12e2b8b4116428d23719
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-09-17 19:54:58 +0530

    Create Which Mixture.py

---
+++ Which Mixture.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    if(a>0 and b>0):
+        print("Solution")
+    elif(a>0 and b==0):
+        print("Solid")
+    else:
+        print("Liquid")
+    
