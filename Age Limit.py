e07773cd0dc0e0f51280b330970df92789dc88bc
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-10-18 14:11:06 +0530

    Create Age Limit.py

---
+++ Age Limit.py
@@ -0,0 +1,7 @@
+# cook your dish here# cook your dish here
+for i in range(int(input())):
+    x,y,a=map(int,input().split())
+    if a>=x  and a<y:
+        print("YES")
+    else:
+        print("NO")
