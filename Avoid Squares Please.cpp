0f62dfdfd34c9561aac2a6786b96b659a48b5d5e
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2022-11-05 22:54:52 +0530

    Create Avoid Squares Please.cpp

---
+++ Avoid Squares Please.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int t;
+	    cin>>t;
+	    while(t){
+	        cout << t <<" ";
+	        t--;
+	    }
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
