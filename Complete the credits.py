0f517cacfaaed210249f2bcfb87824e172cd0fcd
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2023-02-27 12:21:12 +0530

    Create Complete the credits.py

---
+++ Complete the credits.py
@@ -0,0 +1,9 @@
+# cook your dish here
+fot i in range(int(input())):
+    c=int(input())
+    if(c>65):
+        print("overload")
+    elif(c<35):
+        print("underload")
+    else:
+        print("normal")
