b0a9be25ec1f9e1f07784d3a7b5742e5d6b555e4
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2023-04-14 17:19:01 +0530

    Create Candy Division.cpp

---
+++ Candy Division.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i;i<n;i++){
+	   int c;
+	   cin>>c;
+	   if(c%3==0){
+	       cout<<"YES"<<endl;
+	   }
+	   else
+	       cout<<"NO"<<endl;
+	   
+	}
+	return 0;
+}
