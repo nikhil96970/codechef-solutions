43c431c7af03a7888d9ef42572620635aee4513c
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2023-04-15 23:41:11 +0530

    Create Battery Health.cpp

---
+++ Battery Health.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	    int b;
+	    cin>>b;
+	    if(b>=80){
+	        cout<<("YES")<<endl;
+	    }
+	    else
+	        cout<<("NO")<<endl;
+	}
+	return 0;
+}
