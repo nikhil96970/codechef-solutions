074cddc148750794b4b29861aeb1f2959aed985c
Telukuntha Nikhil <83576138+nikhil96970@users.noreply.github.com>
2023-05-22 00:47:53 +0530

    Create Masterchef finals.cpp

---
+++ Masterchef finals.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int i;
+	cin>>t;
+	while(t--){
+	    
+	    cin>>i;
+	    if(i>10){
+	        cout<<"NO"<<endl;
+	        
+	    }
+	    else{
+	    cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
